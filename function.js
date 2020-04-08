511548aac74049c85aa5dc9b5ed70f2553640a4d
mariodandrea <mariodandrea14@yahoo.com>
2020-04-08 19:17:36 -0400

    Created a function that parses through a directory and determines if sub files are folder or files

---
+++ function.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+const projectPath = '/Users/mariodandrea/Documents/Code/graphQL/graphql-basics';
+
+function fileLoop(projectPath) {
+  fs.readdir(projectPath, (err, files) => {
+    if (err) {
+      console.error('Could not list the directory.', err);
+      process.exit(1);
+    }
+
+    console.log(files);
+
+    for (let i = 0; i < files.length; i++) {
+      let filePath = path.join(projectPath, files[i]);
+
+      fs.stat(filePath, (error, stat) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+
+        if (stat.isFile()) {
+          console.log('one file here');
+        } else {
+          fileLoop(filePath);
+        }
+      });
+    }
+  });
+}
+
+fileLoop(projectPath);
